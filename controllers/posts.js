47eae9d8e7152b13e283a99257b71490f0f55d31
Ernest Cheung <ernestcheung02@gmail.com>
2022-06-29 13:42:06 -0700

    users can see individual post

---
+++ controllers/posts.js
@@ -0,0 +1,32 @@
+const Post = require('../models/post');
+module.exports = (app) => {
+	// CREATE
+	app.post('/posts/new', (req, res) => {
+		// INSTANTIATE INSTANCE OF POST MODEL
+		const post = new Post(req.body);
+
+		// SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
+		post.save(() => res.redirect('/'));
+	});
+	// app.post('/posts/new', (req, res) => {
+	// 	console.log(req.body);
+	// });
+	app.get('/', (req, res) => {
+		Post.find({})
+			.lean()
+			.then((posts) => res.render('posts-index', { posts }))
+			.catch((err) => {
+				console.log(err.message);
+			});
+	});
+	app.get('/posts/:id', (req, res) => {
+		Post.findById(req.params.id)
+			.lean()
+			.then((post) => res.render('posts-show', { post }))
+			.catch((err) => {
+				console.log(err.message);
+			});
+	});
+};
+
+// -----
---
+++ server.js
@@ -0,0 +1,45 @@
+// // Initialize express
+const express = require('express');
+const expressValidator = require('express-validator');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// require handlebars
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+const {
+	allowInsecurePrototypeAccess,
+} = require('@handlebars/allow-prototype-access');
+
+require('./controllers/posts')(app);
+require('./data/reddit-db');
+
+// Use "main" as our default layout
+app.engine(
+	'handlebars',
+	exphbs.engine({
+		defaultLayout: 'main',
+		handlebars: allowInsecurePrototypeAccess(Handlebars),
+	})
+);
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+
+// Tell our app to send the "hello world" message to our home page
+app.get('/', (req, res) => {
+	res.render('home', { msg: 'Handlebars are Cool!' });
+});
+
+app.get('/posts/new', (req, res) => {
+	res.render('posts-new');
+});
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+	console.log('App listening on port 3000!');
+});
